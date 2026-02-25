2465624294ead2a83b9a6770bdc9cff7807e0b7b
Dikshant Yadav <dikshant.y22@fms.edu>
2026-02-25 22:24:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: "/dikshant-yadav/",
-  plugins: [react()],
-}) => ({
+
   server: {
     host: "::",
     port: 8080,
@@ -15,7 +13,12 @@ export default defineConfig({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
